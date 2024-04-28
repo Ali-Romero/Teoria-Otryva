5099a0529651e7bbeff2ffb3a3fec74a0805f703
Ali-Romero <sherali920825@yandex.ru>
2024-04-28 17:34:45 +0600

    finances section end

--- source/js/swiper.js
+++ source/js/swiper.js
@@ -1,3 +1,41 @@
+function initSwiperMain() {
+  const swiper = new Swiper('.swiper-main', {
+    speed: 400,
+    effect: 'coverflow',
+    rewind: true,
+    pagination: {
+      el: '.main-pagination',
+      dynamicBullets: true,
+    },
+    breakpoints: {
+      200: {
+        slideToClickedSlide: true,
+        coverflowEffect: {
+          rotate: 50,
+          stretch: 10,
+          depth: 400,
+          scale: 0.8,
+          modifier: 1,
+          slideShadows: false,
+        },
+        allowTouchMove: true,
+      },
+      768: {
+        slideToClickedSlide: false,
+        coverflowEffect: {
+          rotate: 0,
+          stretch: 0,
+          scale: 1,
+          depth: 0,
+          modifier: 0,
+          slideShadows: false,
+        },
+        allowTouchMove: false,
+      },
+    },
+  })
+}
+
 function initSwiperFinances() {
   const swiper = new Swiper('.swiper-finances', {
     speed: 450,
@@ -35,5 +73,6 @@ function initSwiperFinances() {
 }
 
 $(document).ready(function () {
+  initSwiperMain()
   initSwiperFinances()
 })
